255103d7b8e54bf02dfd506c555aafba39b5087e
Carlo Berardelli <owner@MacBook-Pro.fios-router.home>
2020-12-28 22:52:19 -0500

    fixed checkit and backtoport buttons and rearranged icons in navbar

--- components/header.js
+++ components/header.js
@@ -4,9 +4,9 @@ const Header = () => {
         
         <div id="navbar">
             <a href="#top"><h5 id="littlecarlo">Carlo Berardelli</h5></a>
-            <a id="littleimages" href="https://www.linkedin.com/in/carlo-codes/" target="_blank"><img src="/LI-In-Bug.png" alt="GitHub"></img></a>
-            <a id="littleimages" href="https://github.com/CarloRossi11" target="_blank"><img src="/Octocat.png" alt="LinkedIn"></img></a>
             <a id="littleimages" href="https://docs.google.com/document/d/1be8vEZlQsrjrG9OwCjaVmCe-aiceNELO/edit#" target="_blank"><img src="/resume.png" alt="Resume"></img></a>
+            <a id="littleimages" href="https://github.com/CarloRossi11" target="_blank"><img src="/Octocat.png" alt="LinkedIn"></img></a>
+            <a id="littleimages" href="https://www.linkedin.com/in/carlo-codes/" target="_blank"><img src="/LI-In-Bug.png" alt="GitHub"></img></a>
         </div>      
         
         <h1>Carlo Berardelli</h1>
