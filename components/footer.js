e5046e8d7488b2f0952347a0e36b6867ef2949aa
Carlo Berardelli <carloberardelli@yahoo.com>
2021-01-06 18:28:52 -0500

    added gradient header

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(<footer>
-        <p className="smalls">This portfolio site was built on Next.js by Carlo Berardelli</p>
+        <p className="smalls">Portfolio built on Next.js by Carlo Berardelli</p>
     </footer>)
 }
 
--- components/header.js
+++ components/header.js
@@ -7,13 +7,17 @@ const Header = () => {
             <a id="littleimages" href="https://docs.google.com/document/d/e/2PACX-1vQf5_UDHShUSdCai0gtzVKNpqV_OlNReB63InZyEcfk7zLoKuQ5eejJs7IFU9Zc8Q/pub" target="_blank"><img src="/resume.png" alt="Resume"></img></a>
             <a id="littleimages" href="https://github.com/CarloRossi11" target="_blank"><img src="/Octocat.png" alt="LinkedIn"></img></a>
             <a id="littleimages" href="https://www.linkedin.com/in/carlo-codes/" target="_blank"><img src="/LI-In-Bug.png" alt="GitHub"></img></a>
-        </div>      
-        
-        <h1>Carlo Berardelli</h1>
+        </div> 
+
+        <div className="intro">
+        <h1>CARLO BERARDELLI</h1>
         <h3>Full Stack Web Developer</h3>
+        </div>
+
         <a href="https://www.linkedin.com/in/carlo-codes/" target="_blank"><img src="/LI-In-Bug.png" alt="GitHub"></img></a>
         <a href="https://github.com/CarloRossi11" target="_blank"><img src="/Octocat.png" alt="LinkedIn"></img></a>
         <a href="https://docs.google.com/document/d/e/2PACX-1vQf5_UDHShUSdCai0gtzVKNpqV_OlNReB63InZyEcfk7zLoKuQ5eejJs7IFU9Zc8Q/pub" target="_blank"><img src="/resume.png" alt="Resume"></img></a>
+        
 
     </header>)
 }
